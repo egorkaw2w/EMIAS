bdbaeb8b5715c322d667c85ad08eab6bedbe0ca5
Aleksey6060 <145029148+Aleksey6060@users.noreply.github.com>
2024-06-19 13:05:55 +0300

    FRONT epta

---
+++ C#mainproject2/BoolToBackgroundConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace C_mainproject2
+{
+    public class BoolToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isPast = (bool)value;
+            return isPast ? Brushes.LightGray : Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
