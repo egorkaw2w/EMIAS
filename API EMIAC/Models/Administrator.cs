f7295a229f64639514a917fb078663a286daf752
NikolaiSaharov <145434551+NikolaiSaharov@users.noreply.github.com>
2024-06-26 23:47:36 +0300

    12345678

---
+++ API EMIAC/Models/Administrator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_EMIAC.Models;
+
+public partial class Administrator
+{
+    public int? IdAdministrator { get; set; }
+
+    public string AdminSurname { get; set; } = null!;
+
+    public string AdminName { get; set; } = null!;
+
+    public string? AdminMiddleName { get; set; }
+    public string AdminEnterLogin { get; set; } = null!;
+
+    public string AdminEnterPassword { get; set; } = null!;
+}
---
+++ API EMIAC/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_EMIAC.Models;
+
+public partial class Doctor
+{
+    public int? IdDoctor { get; set; }
+
+    public string DoctorSurname { get; set; } = null!;
+
+    public string DoctorName { get; set; } = null!;
+
+    public string? DoctorMiddleName { get; set; }
+
+    public int? SpecialityId { get; set; }
+    public string DoctorEnterLogin { get; set; } = null!;
+
+    public string DoctorEnterPassword { get; set; } = null!;
+
+    public string WorkAddress { get; set; } = null!;
+}
