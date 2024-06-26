018ca61b8367ba1381467d3dd1500d96f70129a3
Aleksey6060 <145029148+Aleksey6060@users.noreply.github.com>
2024-06-26 16:39:20 +0300

    Bichugan

---
+++ C#mainproject2/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_mainproject2
+{
+    public class Appointment
+    {
+        public string PatientName { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public bool IsPast => DateTime.Now > AppointmentTime;
+    }
+}
